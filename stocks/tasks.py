cd38e84bd60360ce62386d9523a6666739861fcd
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-17 09:35:33 -0300

    feat: Replace Selenium with yfinance for fetching stock prices in Celery task

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -1,32 +1,16 @@
-from time import sleep
-
+import yfinance as yf
 from celery import shared_task
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-
 from stocks.models import Stock
-
 
 @shared_task
 def get_stock_price(stock_name):
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(
-        options=options, service=ChromeService(ChromeDriverManager().install())
-    )
-    url = "https://www.google.com"
-    driver.get(url)
-    search_input = driver.find_element(By.XPATH, '//textarea[@aria-label="Pesquisar"]')
-    search_input.send_keys(f"preço da ação {stock_name}")
-    search_input.send_keys(Keys.ENTER)
-    sleep(2)
-    price_div = driver.find_element(By.XPATH, '//div[@data-attrid="Price"]')
-    price = price_div.find_elements(By.TAG_NAME, "span")[2].text.replace(",", ".")
-    driver.quit()
-    Stock.objects.create(name=stock_name, price=float(price))
-    return price
+    stock = yf.Ticker(stock_name)
+    stock_info = stock.history(period="1d")
+    if not stock_info.empty:
+        price = stock_info['Close'].iloc[-1]
+        Stock.objects.create(name=stock_name, price=price)
+        return price
+    else:
+        return None
+
+# get_stock_price("PETR4.SA")
