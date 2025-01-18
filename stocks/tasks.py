5e73585f5a44cfc26a450edc686733a8b6b1da92
Gabriel Moscarde <gabrielmoscarde@outlook.com>
2025-01-17 22:09:27 -0300

    feat: Enhance get_stock_price to handle multiple stock names and refactor price request logic

--- stocks/tasks.py
+++ stocks/tasks.py
@@ -2,15 +2,25 @@ import yfinance as yf
 from celery import shared_task
 from stocks.models import Stock
 
+
 @shared_task
-def get_stock_price(stock_name):
+def get_stock_price(stock_names):
+    if isinstance(stock_names, str):
+        stock_names = [stock_names]
+    
+    prices = [request_stock_price(stock_name) for stock_name in stock_names]
+    return prices
+
+
+def request_stock_price(stock_name):
     stock = yf.Ticker(stock_name)
     stock_info = stock.history(period="1d")
+    
     if not stock_info.empty:
-        price = stock_info['Close'].iloc[-1]
+        price = stock_info["Close"].iloc[-1]
         Stock.objects.create(name=stock_name, price=price)
         return price
-    else:
-        return None
+    return None
+
 
 # get_stock_price("PETR4.SA")
